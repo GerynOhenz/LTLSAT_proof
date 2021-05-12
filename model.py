11621fc8f2b4ee9d4db76fc874ebe3d757340959
GerynOhenz <gerynohenz@qq.com>
2021-05-12 23:35:28 +0800

    update beam search

--- model.py
+++ model.py
@@ -52,7 +52,7 @@ class TransformerModel(nn.Module):
 		src = self.src_embedding(sources)  # (N, S, E)
 		src = src.transpose(0, 1)  # (S, N, E)
 		# get mask
-		batch_size, sources_len = sources.size()
+		batch_size, sources_len = sources.shape
 		src_key_padding_mask = self.pad_masking(source)  # (N, S)
 		memory_key_padding_mask = src_key_padding_mask  # (N, S)
 		memory = self.transformer_encoder(src, src_key_padding_mask=src_key_padding_mask)
@@ -60,7 +60,7 @@ class TransformerModel(nn.Module):
 
 	def decode(self, targets, memory, memory_key_padding_mask):
 		# (N, T)
-		batch_size, targets_len = targets.size()
+		batch_size, targets_len = targets.shape
 		# embedding
 		tgt = self.tgt_embedding(targets)
 		tgt = tgt.transpose(0, 1)  # (T, N, E)
@@ -142,7 +142,7 @@ class Model_with_Proof(nn.Module):
 				edge_index,
 				edge_label):
 
-		batch_size=source.size()[0]
+		batch_size=source.shape[0]
 		device=source.device()
 
 		encode_output, decode_output, output_logits=self.transformer(source, target[:, :-1])
@@ -219,69 +219,53 @@ class Evaluator:
 		self.len_penalty=len_penalty
 
 	def beam_search(self, source):
-		batch_size=source.size()[0]
+		batch_size=source.shape[0]
 		device=source.device()
+		source_beam=source_beam.repeat_interleave(self.n_beam, dim=0)
 
 		with torch.no_grad():
-			beam_scores=torch.zeros(batch_size*self.n_beam).to(device)
-			best_scores=[-1e9]*batch_size
-			best_gen=[[] for i in range(batch_size)]
-			done=torch.zeros(batch_size*self.n_beam).to(device)
+			scores=torch.zeros(batch_size*self.n_beam).to(device)
+			done=torch.full(batch_size*self.n_beam, False, dtype=torch.bool, device=device)
 
-			gen_seq=torch.full((batch_size*n_beam, 1), self.tgt_sos_idx, type=torch.int32)
+			gen_seq=torch.full((batch_size*n_beam, 1), self.tgt_sos_idx, dtype=torch.int32, device=device)
 
 			for i in range(self.max_seq_len):
 				if done.sum().item()==self.batch_size*self.n_beam:
 					break
 
-				_, _, output_logits=self.model.transformer(source, gen_seq)
-				output_logits=output_logits[:, -1, :]
-				scores=F.log_softmax(output_logits, dim=-1)
-				next_scores=scores+beam_scores[:, None].expand_as(scores)
-				next_scores=next_scores.view(batch_size, -1)
-				next_scores, next_tokens=torch.topk(next_scores, n_beam, dim=1, largest=True, sorted=True)
+				_, _, output_logits=self.model.transformer(source_beam, gen_seq)
+				best_k2_probs, best_k2_idx=output_logits[:, -1, :].topk(self.n_beam)
+				scores=F.log_softmax(best_k2_probs, dim=-1).masked_fill(done[:, None], 0)+scores[:, None]
 
-				next_beam_search=[]
+				neg_inf_mask = (torch.BoolTensor([False]+[True]*(self.n_beam-1)).to(device)).repeat(batch_size*self.n_beam, 1).logical_and(done[:, None])
+				scores=scores+torch.zeros(neg_inf_mask.shape, device=neg_inf_mask.device).masked_fill(neg_inf_mask, -1e6)
 
-				for batch_index in range(batch_size):
-					if done[batch_index]:
-						next_beam_search.extend([(0, self.tgt_pad_idx, 0)]*self.n_beam)
-						continue
+				scores, best_k_idx_in_k2=torch.topk(scores.view(batch_size, -1), self.n_beam, dim=-1, largest=True, sorted=True)
 
-					flag=True
-					for beam_token_rank, (beam_token_id, beam_token_score) in enumerate(zip(next_tokens[batch_index], next_scores[batch_index])):
-						beam_id=beam_token_id//self.n_tgt_vocab
-						token_id=beam_token_id%self.n_tgt_vocab
+				scores=scores.view(-1)
+				best_k_idx=best_k2_idx.view(batch_size, -1)[best_k_idx_in_k2].view(-1) #token
+				best_r_idx=best_k_idx_in_k2 // self.n_beam #beam resort
+				done=done[best_r_idx]
 
-						if token_id.item()==self.tgt_eos_idx:
-							if beam_token_score.item()/(i+1)**self.len_penalty>best_score[batch_index]:
-								best_score[batch_index]=beam_token_score.item()/(i+1)**self.len_penalty
-								best_gen[batch_index]=gen[batch_index*n_beam+beam_id][1:].cpu().detach().numpy().tolist()
-							
-							next_beam_search.append([(0, self.tgt_pad_idx, 0)])
-						else:
-							flag=False
-							next_beam_search.append([(beam_token_score, token_id, batch_index*n_beam+beam_id)])
+				best_k_idx=best_k_idx.masked_fill(done, self.tgt_pad_idx)
+				done=done.logical_or(best_k_idx==self.tgt_eos_idx)
 
-				beam_scores=torch.IntTensor([x[0] for x in next_beam_search]).to(device)
-				beam_tokens=torch.IntTensor([x[1] for x in next_beam_search]).to(device)
-				beam_idx=torch.IntTensor([x[2] for x in next_beam_search]).to(device)
+				gen_seq=torch.cat((gen_seq[best_r_idx, :], best_k_idx.unsquenze(dim=1)), dim=-1)
 
-				gen_seq=torch.cat((gen_seq[beam_idx, :], beam_tokens.unsquenze(dim=1)), dim=-1)
+			target=gen_seq[torch.arange(0, batch_size*self.n_beam, self.n_beam).to(device), :]
 
-				done=(gen_seq == self.tgt_eos_idx).sum(dim=1)>0
-
-			for i in range(batch_size*n_beam):
-				batch_index=i//n_beam
-				if beam_scores[i].item()/self.max_seq_len**self.len_penalty>best_score[batch_index]:
-					best_score[batch_index]=beam_scores[i].item()/self.max_seq_len**self.len_penalty
-					best_gen[batch_index]=gen[i][1:].cpu().detach().numpy().tolist()
-
-			target=best_gen
 			state_len=[]
 			for x in target:
 				target_offset.append([0])
-				for y in x.split(";"):
+				x_list=""
+				for y in x:
+					if y==self.tgt_eos_idx:
+						break
+					if y==self.tgt_semicolon_idx:
+						x_list+=';'
+					x_list+='#'
+
+				for y in x.split(';'):
 					target_offset[-1].append(target_offset[-1][-1]+len(y)+1)
 				target_offset[-1][-1]-=1
 				state_len.append(len(target_offset[-1])-1)
@@ -289,13 +273,12 @@ class Evaluator:
 			max_target_len=max(target)
 			max_state_len=max(state_len)+1
 			for i in range(batch_size):
-				target[i]+=[self.src_pad_idx]*(max_target_len-len(target[i]))
 				target_offset[i]+=[0]*(max_state_len-len(target_offset[i]))
 				
-			return target, state_len, target_offset
+			return target, torch.IntTensor(state_len).to(device), torch.IntTensor(target_offset).to(device)
 
 	def gen_edge_index(self, source_len, state_len, right_pos):
-		batch_size=source_len.size()[0]
+		batch_size=source_len.shape[0]
 		n_node=source_len.max()*state_len.max()
 		edge_index_list=[]
 
@@ -320,50 +303,51 @@ class Evaluator:
 	def run(self, source, source_len):
 
 		target, state_len, target_offset=self.beam_search(source)
-		batch_size=source.size()[0]
+		batch_size=source.shape[0]
 		device=source.device()
 
-		encode_output, decode_output, output_logits=self.model.transformer(source, target[:, :-1])
+		with torch.no_grad():
+			encode_output, decode_output, output_logits=self.model.transformer(source, target[:, :-1])
 
-		left_encode=self.model.blockleft(encode_output[:, 1:])
-		right_encode=self.model.blockright(encode_output[:, 1:])
-		block_match=torch.matmul(left_encode, right_encode.transpose(1, 2))
+			left_encode=self.model.blockleft(encode_output[:, 1:])
+			right_encode=self.model.blockright(encode_output[:, 1:])
+			block_match=torch.matmul(left_encode, right_encode.transpose(1, 2))
 
-		right_pos=block_match.argmax(dim=-1)
+			right_pos=block_match.argmax(dim=-1)
 
-		n_node=source_len.max()*state_len.max()
+			n_node=source_len.max()*state_len.max()
 
-		edge_index=self.gen_edge_index(source_len, state_len, right_pos)
+			edge_index=self.gen_edge_index(source_len, state_len, right_pos)
 
-		block_embedding=torch.zeros((batch_size, source_len.max(), self.d_model)).to(device)
+			block_embedding=torch.zeros((batch_size, source_len.max(), self.d_model)).to(device)
 
-		for i in range(batch_size):
-			for j in range(source_len[i]):
-				block_embedding[i][j]=encode_output[i][1+j:1+right_pos[i][j]].mean(dim=-1)
+			for i in range(batch_size):
+				for j in range(source_len[i]):
+					block_embedding[i][j]=encode_output[i][1+j:1+right_pos[i][j]].mean(dim=-1)
 
-		trace_embedding=torch.zeros((batch_size, state_len.max(), self.d_model)).to(device)
+			trace_embedding=torch.zeros((batch_size, state_len.max(), self.d_model)).to(device)
 
-		for i in range(batch_size):
-			for j in range(state_len[i]):
-				block_embedding[i][j]=decode_output[i][target_offset[i][j]:target_offset[i][-1]].mean(dim=-1)
+			for i in range(batch_size):
+				for j in range(state_len[i]):
+					block_embedding[i][j]=decode_output[i][target_offset[i][j]:target_offset[i][-1]].mean(dim=-1)
 
-		node_embedding=torch.cat((torch.repeat_interleave(block_embedding, state_len.max(), dim=1),
-								torch.repeat_interleave(trace_embedding, source_len.max(), dim=1)),
-								dim=-1)
+			node_embedding=torch.cat((torch.repeat_interleave(block_embedding, state_len.max(), dim=1),
+									torch.repeat_interleave(trace_embedding, source_len.max(), dim=1)),
+									dim=-1)
 
-		node=self.model.P_node(node_embedding).argmax(dim=-1)
-		node_mask=node==2
+			node=self.model.P_node(node_embedding).argmax(dim=-1)
+			node_mask=node==2
 
-		edge_embedding=torch.zeros((batch, n_node*6, self.d_model*6)).to(device)
+			edge_embedding=torch.zeros((batch, n_node*6, self.d_model*6)).to(device)
 
-		for i in range(batch_size):
-			for j in range(n_node):
-				head=node_embedding[i][j].repeat(6, 1)
-				tail=node_embedding[i].index_select(dim=0, edge_index[i][j])
-				edge_embedding[i][j*6:(j+1)*6]=torch.cat((head, tail, head-tail), dim=-1)
+			for i in range(batch_size):
+				for j in range(n_node):
+					head=node_embedding[i][j].repeat(6, 1)
+					tail=node_embedding[i].index_select(dim=0, edge_index[i][j])
+					edge_embedding[i][j*6:(j+1)*6]=torch.cat((head, tail, head-tail), dim=-1)
 
-		edge, edge_choice=torch.topk(self.model.P_edge(edge_embedding), 2, dim=-1, largest=True, sorted=True)
-		edge_choice=edge_index[edge_idx]
+			edge, edge_choice=torch.topk(self.model.P_edge(edge_embedding), 2, dim=-1, largest=True, sorted=True)
+			edge_choice=edge_index[edge_idx]
 
 		max_source_len=source_len.max()
 
